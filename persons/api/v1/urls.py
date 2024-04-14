1a0d3482e9dc7aa24fe9864dc2440009f0996bdf
Marcus Vinicus Braga <mvbraga@gmail.com>
2024-04-14 03:53:39 -0300

    Novos arquivos

--- persons/api/v1/urls.py
+++ persons/api/v1/urls.py
@@ -2,10 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from persons.api.v1 import views
+from persons.api.v1.views import CheckPersonAPIView, CreatePersonAPIView, AsyncCreatePersonAPIView
 
 router = DefaultRouter()
 router.register(r'persons', views.PersonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('check/', CheckPersonAPIView.as_view(), name='check'),
+    path('create_bulk/', CreatePersonAPIView.as_view(), name='create-bulk'),
+    path('fetch_data/', AsyncCreatePersonAPIView.as_view(), name='fetch-data'),
 ]
--- persons/api/v1/views.py
+++ persons/api/v1/views.py
@@ -1,4 +1,14 @@
-from rest_framework import viewsets
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+
+import aiohttp
+from django.http import JsonResponse
+from django.views.generic import View
+from loguru import logger
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from persons.api.v1.serializers import PersonSerializer
 from persons.models import Person
@@ -7,3 +17,64 @@ from persons.models import Person
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+
+class CheckPersonAPIView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(self.get_person, request)
+            response = future.result()
+        return response
+
+    def get_person(self, request):
+        pk = request.query_params.get('pk', None)
+        person = Person.objects.get(pk=pk)
+        serializer = PersonSerializer(person)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CreatePersonAPIView(APIView):
+
+    def create_persons(self):
+        logger.debug("Executando...")
+        for i in range(1000):
+            Person.objects.create(name=f'Person {i}', age=i)
+            sleep(0.5)
+        logger.debug("Finalizado...")
+
+    def create_bulk(self, request):
+        logger.debug("Iniciando a criação...")
+        self.create_persons()
+        return Response({"message": "1000 persons created"}, status=status.HTTP_201_CREATED)
+
+    def get(self, request, *args, **kwargs):
+        logger.debug("Cheguei no GET...")
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(self.create_bulk, request)
+            response = future.result()
+        return response
+
+
+class AsyncCreatePersonAPIView(View):
+    async def fetch_data(self, session, url):
+        logger.debug("Cheguei no fetch_data...")
+        async with session.get(url) as response:
+            return await response.json()
+
+    async def get(self, request, *args, **kwargs):
+        logger.debug("Cheguei no GET...")
+        urls = [
+            "https://jsonplaceholder.typicode.com/todos/1",
+            "https://jsonplaceholder.typicode.com/todos/2",
+            "https://jsonplaceholder.typicode.com/todos/3",
+            "https://jsonplaceholder.typicode.com/todos/2",
+            "https://jsonplaceholder.typicode.com/todos/1",
+        ]
+        logger.debug("Iniciando...")
+        async with aiohttp.ClientSession() as session:
+            tasks = [self.fetch_data(session, url) for url in urls]
+            results = await asyncio.gather(*tasks, return_exceptions=True)
+
+        logger.debug("Finalizando...")
+        return JsonResponse({"results": results})
